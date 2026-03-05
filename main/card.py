2f294d562a07efa641630846d431ebcb7df17eb9
ummfieg <ummfieg@naver.com>
2026-03-06 04:36:53 +0900

    feat: 카드 수정 api 구현

--- main/card.py
+++ main/card.py
@@ -7,6 +7,7 @@ from utils import to_object_id
 bp = Blueprint('card',__name__)
 
 @bp.route("/api/cards", methods=["GET"])
+#페이지 네이션 적용
 def get_cards():
     sort_param = request.args.get("sort","latest")
     SORT_MAP ={
@@ -16,7 +17,7 @@ def get_cards():
     }
     field, order = SORT_MAP.get(sort_param, ("created_at", -1))
     try:
-        cards = list(db.jungle_wiki.find({}).sort(field, order))
+        cards = list(db.card.find({}).sort(field, order))
         return jsonify({"result": "success", "data": serialize_id(cards)}), 200
     except Exception as e:
         print(e)
@@ -44,9 +45,8 @@ def card_post():
         # "author":user_id
     }
 
-    
     try: 
-        result = db.jungle_wiki.insert_one(card)
+        result = db.card.insert_one(card)
         return jsonify({"result":"sucess", "msg":"등록이 완료되었습니다."}),200
     
     except Exception as e:
@@ -60,7 +60,7 @@ def search_card():
     if not keyword:
         return jsonify({"result":"fail", "msg":"검색어를 입력해주세요"}), 401
     try:
-        find_cards = list(db.jungle_wiki.find({"title":{"$regex":keyword, "#options": "i"} },{"_id":1, "title":1}))
+        find_cards = list(db.card.find({"title":{"$regex":keyword, "#options": "i"} },{"_id":1, "title":1}))
       
 
         if not find_cards:
@@ -71,8 +71,37 @@ def search_card():
         return jsonify({"result":"fail", "msg":"서버오류"}),500
         
 
+@bp.route("/api/cards/<cardId>", methoeds=["PATCH"])
+def card_edit(cardId):
 
+    input_data = request.get_json()
+    title = input_data.get("title")
+    content = input_data.get("content")
 
+    try:
+        if not title or not content:
+            return jsonify({"result":"fail","msg":"제목과 내용은 필수입니다."}),400
+        
+        find_edit = db.card.find_one({"_id":to_object_id(cardId)})
+    
+        obj_id = to_object_id(cardId)
 
+        if not obj_id:
+            return jsonify({"result":"fail","msg":"잘못된 id"}),400
+    
+        find_edit = db.card.find_one({"_id": obj_id})
+        if not find_edit:
+            return jsonify({"result":"fail","msg":"해당 카드 없음"}),404
+        
+        if title == find_edit.get("title") and content == find_edit.get("content"):
+            return jsonify({"result":"fail", "msg":"변경된 내용이 없습니다."}), 400
 
-
+        result = db.card.update_one({"_id": obj_id},{"$set":{"title": title,"content": content}, "$inc":{ "version":1 }})
+        if result.matched_count == 1:
+            return jsonify({"result":"success", "msg":"수정 완료"}),200
+        else:
+            return jsonify({"result":"fail", "msg":"수정 실패"}), 500
+    except Exception as e: 
+        print(e)
+        return jsonify({ "result":"fail", "msg":"서버오류"}), 500
+    
