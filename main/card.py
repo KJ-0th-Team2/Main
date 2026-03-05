728c96ec8168a8ea534f72476d0362e8781cd11a
ummfieg <ummfieg@naver.com>
2026-03-05 19:41:12 +0900

    refactor: 유틸함수 적용 및 상태코드 수정

--- main/card.py
+++ main/card.py
@@ -1,7 +1,7 @@
 from flask import render_template, Blueprint, request, jsonify
 from pymongo import MongoClient
 from datetime import datetime
-
+from utils import serialize_id
 
 client = MongoClient('localhost', 27017)
 db = client.jungle_wiki
@@ -9,15 +9,19 @@ bp = Blueprint('card',__name__)
 
 @bp.route("/cards", methods=["GET"])
 def get_cards():
-    sort_pram = request.args.get("sort","latest")
+    sort_param = request.args.get("sort","latest")
     SORT_MAP ={
         "latest": ("created_at", -1),
         "views": ("view_count",-1),
         "comments" :("comment_count", -1)
     }
-    field, order = SORT_MAP.get(sort_pram, ("created_at", -1))
-    cards = list(db.jungle_wiki.find({}).sort(field, order))
-    return jsonify(cards)
+    field, order = SORT_MAP.get(sort_param, ("created_at", -1))
+    try:
+        cards = list(db.jungle_wiki.find({}).sort(field, order))
+        return jsonify({"result": "success", "data": serialize_id(cards)}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"result": "fail", "msg": "서버 오류"}), 500
 
 @bp.route("/cards", methods=["POST"])
 # 로그인 상태 데코레이터함수 넣기
@@ -35,7 +39,8 @@ def card_post():
         "title":title,
         "content":content,
         "version":1,
-        "created_at": datetime.now()
+        "created_at": datetime.now(),
+        "comment_count":0,
         # "author":user_id
     }
 
@@ -56,16 +61,13 @@ def search_card():
         return jsonify({"result":"fail", "msg":"검색어를 입력해주세요"}), 401
     try:
         find_cards = list(db.jungle_wiki.find({"title":{"$regex":keyword, "#options": "i"} },{"_id":1, "title":1}))
-        for card in find_cards:
-            # Objdctd는 JSON 직렬화 불가로 str변환 필요
-            card["_id"] = str(card["_id"]) 
+      
 
         if not find_cards:
             return jsonify({"result":"fail", "msg":"검색 결과가 없음"}), 404
         
-        return jsonify({"result":"success", "msg":"해당카드 찾기 성공", "data":find_cards}), 200
+        return jsonify({"result":"success", "msg":"해당카드 찾기 성공", "data":serialize_id(find_cards)}), 200
     except:
         return jsonify({"result":"fail", "msg":"서버오류"}),500
         
 
- 
