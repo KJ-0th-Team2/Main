54d2cd185bdcdf4cf000d26c40a62ab3f2e98833
ummfieg <ummfieg@naver.com>
2026-03-05 19:11:51 +0900

    fix: 키값 수정

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@
 def serialize_id(data):
     if isinstance(data,list):
         for item in data:
-            item["_id"] = str(item["id"])
+            item["_id"] = str(item["_id"])
     else:
         data["_id"] = str(data["_id"])
     return data
