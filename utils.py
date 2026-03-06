90296bd8d06d26360789ebd56b13c32234066114
이창원 <gravity980530@gmail.com>
2026-03-06 09:37:59 +0900

    프론트엔드 정규식 제거

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from bson import ObjectId
+import re
 
 # ObjdectId 직렬화 
 def serialize_id(data):
@@ -19,3 +20,9 @@ def to_object_id(id):
     try:
         return ObjectId(id)
     except: return None
+
+def strip_markdown(text):
+    text = re.sub(r'<br\s*/?>', '', text)
+    text = re.sub(r'[*_~`#><]', '', text)  # 기호 제거
+    text = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', text)  # 링크 제거
+    return text.strip()
